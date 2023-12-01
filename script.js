726a9af2e2c1ca03683d38a5dcdc5b79a3484b61
Bobby <124845332+7DatsonBack@users.noreply.github.com>
2023-12-01 15:31:08 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+document.addEventListener('scroll', function() {
+    var scrollingText = document.getElementById('scrolling-text');
+    var scrollingContainer = document.getElementById('scrolling-container');
+    var scrollPosition = window.scrollY;
 
+ 
+    if (scrollPosition > 300) {
+      
+        scrollingText.style.opacity = 1;
+      
+    } else {
+      
+        scrollingText.style.opacity = 0;
+      
+    }
+});
